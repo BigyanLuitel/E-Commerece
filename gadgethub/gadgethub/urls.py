1ce84b27f36b5670f393ea8702ee924f24fc7e2d
Bigyan <luitelbigyan344@gmail.com>
2026-07-22 21:20:47 +0545

    cart

--- gadgethub/gadgethub/urls.py
+++ gadgethub/gadgethub/urls.py
@@ -22,8 +22,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
+        path('cart/', include('cart.urls', namespace='cart')),
+
     path('', include('products.urls', namespace='products')),
-    path('cart/', include('cart.urls', namespace='cart'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
